8dee91d0419963b9d33de85bb839494a529fa107
Birkás Bence <birkasbence99@gmail.com>
2022-05-12 21:41:08 +0200

    weapon now rotates vertically

--- grafika/feladat/source/move.c
+++ grafika/feladat/source/move.c
@@ -2,22 +2,6 @@
 #include "scene.h"
 #include "camera.h"
 
-/*void moveItem(scene *scene)
-{
-    if (abs((int)(camera.position.x - scene->weapon.position.x)) < 70 && abs((int)(camera.position.z - scene->weapon.position.z)) < 70 && camera.position.y < 200)
-    {
-        double angle = degree_to_radian(camera.pose.z);
-        double angle2 = degree_to_radian(camera.pose.z + 130);
-
-        scene->weapon.position.x = camera.position.x - sin(angle) * 22 - sin(angle2) * -30;
-        scene->weapon.position.y = camera.position.y - 20;
-        scene->weapon.position.z = camera.position.z - cos(angle) * 22 - cos(angle2) * -30;
-        // printf("%lf\n", camera.pose.x);
-        // scene->weapon.angle.x = -camera.pose.x;
-        scene->weapon.angle.y = fmod((270 + camera.pose.z), 360);
-    }
-}*/
-
 void moveItem(Scene *scene, double time, Camera *camera)
 {
     // if (abs((int)(camera->position.x - scene->weapon.position.x)) < 100 && abs((int)(camera->position.y - scene->weapon.position.z)) < 100)
@@ -30,6 +14,5 @@ void moveItem(Scene *scene, double time, Camera *camera)
     scene->weapon.position.z = -camera->position.y - (cos(angle) * -10) - (cos(angle2) * 10);
 
     scene->weapon.rotation.y = fmod((camera->rotation.z + 90), 360);
-    // printf("x: %.2f | z: %.2f || r: %.2f\n", scene->weapon.position.x, scene->weapon.position.z, scene->weapon.rotation.y);
-    // scene->weapon.rotation.x = fmod((camera->rotation.x), 360);
+    scene->weapon.rotation.x = -fmod((camera->rotation.x), 360);
 }
